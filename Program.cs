297da81fb9cff8961f7151ee88b7978f912289f7
Safeya-Yasien <bluesky7yas@gmail.com>
2023-08-03 03:19:41 +0300

    user class

--- Program.cs
+++ Program.cs
@@ -11,14 +11,29 @@ namespace FinallProject
     {
         static void Main(string[] args)
         {
-            FunTravel f1 = new FunTravel(1, 10, "10 july", "cairo", 1000, "Maka", true);
-            Console.WriteLine(f1.Datails());
+            //FunTravel f1 = new FunTravel(1, 10, "10 july", "cairo", 1000, "Maka", true);
+            //Console.WriteLine(f1.Datails());
 
-            RelTravel r1 = new RelTravel(2, 5, "3 october", "alx", 4000, "madina", "bus");
-            Console.WriteLine(r1.Datails());
+            //RelTravel r1 = new RelTravel(2, 5, "3 october", "alx", 4000, "madina", "bus");
+            //Console.WriteLine(r1.Datails());
 
-            InTravel i1 = new InTravel(3, "2 february", 9, "ghana", 1500, "bus");
-            Console.WriteLine(i1.Datails());
+            //InTravel i1 = new InTravel(3, "2 february", 9, "ghana", 1500, "bus");
+            //Console.WriteLine(i1.Datails());
+
+            Console.WriteLine("Username: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Password: ");
+            string password = Console.ReadLine();
+
+            if (User.Login(username, password))
+            {
+                Console.WriteLine("Login successful!");
+            }
+            else
+            {
+                Console.WriteLine("Login failed. Invalid username or password.");
+            }
 
 
         }
--- User.cs
+++ User.cs
@@ -8,17 +8,36 @@ namespace FinallProject
 {
     internal static class User
     {
-        private static string[] UserNames = { "user1", "user2", "user3", "user4", "user5" };
-        private static string[] Passwords = { "pass1", "pass2", "pass3", "pass4", "pass5" };
+        //private static string[] UserNames = { "user1", "user2", "user3", "user4", "user5" };
+        //private static string[] Passwords = { "pass1", "pass2", "pass3", "pass4", "pass5" };
+
+        //public static bool Login(string username, string password)
+        //{
+        //    for (int i = 0; i < UserNames.Length; i++)
+        //    {
+        //        if (UserNames[i] == username && Passwords[i] == password)
+        //        {
+        //            return true;
+        //        }
+        //    }
+
+        //    return false;
+        //}
+
+        private static Dictionary<string, string> UserCredentials = new Dictionary<string, string>
+        {
+            { "user1", "pass1" },
+            { "user2", "pass2" },
+            { "user3", "pass3" },
+            { "user4", "pass4" },
+            { "user5", "pass5" }
+        };
 
         public static bool Login(string username, string password)
         {
-            for (int i = 0; i < UserNames.Length; i++)
+            if (UserCredentials.ContainsKey(username) && UserCredentials[username] == password)
             {
-                if (UserNames[i] == username && Passwords[i] == password)
-                {
-                    return true;
-                }
+                return true;
             }
 
             return false;
